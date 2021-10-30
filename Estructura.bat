c1199cc99642751e447e2068934e7b6324a9d82e
Oscar Lopez <43121128+tod1504@users.noreply.github.com>
2021-10-29 20:34:55 -0700

    Create Estructura.bat

---
+++ Estructura.bat
@@ -0,0 +1,5 @@
+@echo off
+echo "Los archivos del programa se encuentran dentro de la carpeta src"
+echo "Se crean los archivos del front end, empezando con el index en la raiz y los otros componentes en la carpeta 'componentes', cada componente teniendo su propia carpeta y los archivos de estilos dentro de la carpeta 'estilos' ubicada en la raiz"
+echo "Se crean archivos de back end para complementar archivos del front end, estos archivos se encontraran en la misma carpeta que sus archivos html correspondientes"
+set/p arg ="Presiona cualquier tecla para cerrar"
