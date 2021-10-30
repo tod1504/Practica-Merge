97cd10ec1fe87a392d4c69fe4c8e1830aeb9b0f1
Oscar Lopez <43121128+tod1504@users.noreply.github.com>
2021-10-29 18:51:01 -0700

    Create Estructura.bat
    Creacion de ejecutable

---
+++ Estructura.bat
@@ -0,0 +1,6 @@
+@echo off
+echo "Se crea el branch main"
+echo "Se crean los archivos del front end en branch nueva, empezando con el index en la raiz y los otros componentes en la carpeta 'componentes', con los archivos de estilos dentro de la carpeta 'estilos'"
+echo "Se crean archivos de back end para complementar archivos del front end, estos archivos se encontraran en la misma carpeta que los archivos html"
+echo "Se realiza merge de los branchs creados"
+set/p arg ="Presiona cualquier tecla para cerrar"
